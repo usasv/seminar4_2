070b2aa889fc44b0e28282417d15a4fd0e7e6be9
Sardor Nazarov <00014384@wiut.uz>
2022-10-06 14:14:14 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def encode(message):
+    encoded_message = ""
+    i = 0
+    while i <= len(message)-1:
+        count = 1
+        char = message[i]
+        j = i
+        while j < len(message) - 1:
+            if message[j] == message[j + 1]:
+                count = count + 1
+                j = j + 1
+            else:
+                break
+        encoded_message = encoded_message + "*" + char + str(count)
+        i = j + 1
+    return encoded_message
+
+encoded_message = encode("ABBBBCCCCCCCCAB")
+print(encoded_message)
